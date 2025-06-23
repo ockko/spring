50d8bf96bed6598745189f91218d569a7f1ed8ba
ockko <ocko119@gmail.com>
2025-06-24 02:26:22 +0900

    [연습] ServletConfig, WebConfig, HomeController

--- study/spring/ex01/src/main/java/org/scoula/config/RootConfig.java
+++ study/spring/ex01/src/main/java/org/scoula/config/RootConfig.java
@@ -4,4 +4,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RootConfig {
+
+    // db 사용 --> google mybatis 사용 설정
+    // 트랜잭션 설정
 }
--- study/spring/ex01/src/main/java/org/scoula/config/ServletConfig.java
+++ study/spring/ex01/src/main/java/org/scoula/config/ServletConfig.java
@@ -2,11 +2,36 @@ package org.scoula.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-// mvc 관련된 설정이나 객체생성 + 주입
-@EnableWebMvc // mvc 관련된 설정을 할 수 있는 파일로 등록
-@ComponentScan(basePackages = "org.scoula.controller")
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.scoula.controller"})
 public class ServletConfig implements WebMvcConfigurer {
-    // 어떤 함수를 써서 설정해야 할지 이미 WebMvcConfigurer에 정의되어 있음.
+
+    // 프론트파일(css, js, img)의 위치를 지정해주는 함수
+    // /resources/img/a.png라고 요청이 들어오면
+    // /resources/ 밑에서 찾겠다라는 설정
+    // <img src="/resources/img/a.png">
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    // jsp view resolver 설정
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+
+        registry.viewResolver(bean);
+    }
 }
