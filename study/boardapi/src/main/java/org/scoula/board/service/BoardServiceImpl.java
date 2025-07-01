e58928fa330ec4980813679cb7ef8959a06d4dff
ockko <ocko119@gmail.com>
2025-07-01 18:01:23 +0900

    [연습] Rest 컨트롤러 준비

---
+++ study/boardapi/src/main/java/org/scoula/board/service/BoardServiceImpl.java
@@ -0,0 +1,90 @@
+package org.scoula.board.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.board.domain.BoardAttachmentVO;
+import org.scoula.board.domain.BoardVO;
+import org.scoula.board.dto.BoardDTO;
+import org.scoula.board.mapper.BoardMapper;
+import org.scoula.common.util.UploadFiles;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Log4j2
+@Service // @Component + 서비스 역할의 클래스라는 것이 스프링에 등록
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    private final static String BASE_DIR = "c:/upload/board";
+    // 전처리해서 dao의 메서드를 불러서 db 처리해달라고 해야 함.
+    private final BoardMapper boardMapper;
+
+    @Override
+    public List<BoardDTO> getList() {
+        return boardMapper.getList().stream() // BoardVO의 스트림
+                .map(BoardDTO::of) // BoardDTO의 스트림
+                .toList(); // List<BoardDTO> 변환
+    }
+
+    @Override
+    public BoardDTO get(Long no) {
+        BoardVO vo = boardMapper.get(no);
+        // 다른 곳으로 넘길 때는 dto로 만들어서 넘기자.
+        BoardDTO dto = BoardDTO.of(vo);
+        return Optional.ofNullable(dto)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    @Transactional
+    @Override
+    public BoardDTO create(BoardDTO board) {
+        BoardVO vo = board.toVo();
+        boardMapper.create(vo);
+
+        List<MultipartFile> files = board.getFiles();
+        if (files != null && !files.isEmpty()) {
+            upload(vo.getNo(), files);
+        }
+
+        return get(vo.getNo());
+    }
+
+    private void upload(Long bno, List<MultipartFile> files) {
+        for (MultipartFile part : files) {
+            if (part.isEmpty()) continue;
+            try {
+                String uploadPath = UploadFiles.upload(BASE_DIR, part);
+                BoardAttachmentVO attach = BoardAttachmentVO.of(part, bno, uploadPath);
+                boardMapper.createAttachment(attach);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public BoardDTO update(BoardDTO board) {
+        return boardMapper.update(board.toVo()) == 1;
+    }
+
+    @Override
+    public BoardDTO delete(Long no) {
+        return boardMapper.delete(no) == 1;
+    }
+
+    @Override
+    public BoardAttachmentVO getAttachment(Long no) {
+        return boardMapper.getAttachment(no);
+    }
+
+    @Override
+    public boolean deleteAttachment(Long no) {
+        return boardMapper.deleteAttachment(no) == 1;
+    }
+}
---
+++ study/boardapi/src/main/java/org/scoula/common/util/UploadFiles.java
@@ -0,0 +1,43 @@
+package org.scoula.common.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+
+public class UploadFiles {
+
+    public static String upload(String baseDir, MultipartFile part) throws IOException {
+        File base = new File(baseDir);
+        if (!base.exists()) {
+            base.mkdirs();
+        }
+        String filename = part.getOriginalFilename();
+        File dest = new File(baseDir, UploadFileName.getUniqueName(filename));
+        part.transferTo(dest);
+        return dest.getPath();
+    }
+
+    public static String getFormatSize(Long size) {
+        if (size <= 0) return "0";
+        final String[] units = new String[]{"Bytes", "KB", "MB", "GM", "TB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+
+    public static void download(HttpServletResponse response, File file, String orgName) throws Exception {
+        response.setContentType("application/download");
+        response.setContentLength((int) file.length());
+        String filename = URLEncoder.encode(orgName, "UTF-8");
+        response.setHeader("Content-disposition", "attachment;filename=\"" + filename + "\"");
+        try (OutputStream os = response.getOutputStream();
+             BufferedOutputStream bos = new BufferedOutputStream(os)) {
+            Files.copy(Paths.get(file.getPath()), bos);
+        }
+    }
+}
