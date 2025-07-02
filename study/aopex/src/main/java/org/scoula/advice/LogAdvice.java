8a89d7015a8f13bffb90a9fc59c6cb9eaf23134e
ockko <ocko119@gmail.com>
2025-07-02 17:47:11 +0900

    [연습] AOP

---
+++ study/aopex/src/main/java/org/scoula/advice/LogAdvice.java
@@ -0,0 +1,26 @@
+package org.scoula.advice;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Log4j2
+@Component
+public class LogAdvice {
+
+    // 어떤 타겟이 되는 클래스의 메서드를 호출해서 실행할 때
+    // 어떤 부가적인 기능을 넣을지 충고해주는 클래스
+    // ==> 클래스 이름 위에 @Aspect 라고 해주어야 함.
+
+    @Before("execution(* org.scoula.sample.service.SampleService*.*(..))")
+    public void logBefore() {
+        log.info("========================================");
+    }
+
+    // 부가적인 기능을 넣을 타겟이 되는 SampleService 클래스 안에 있는
+    // 조인포인트가 되는 모든 메서드를 호출해서 실행하기 전에(이벤트라고 인식함.)
+    // logBefore() 메서드를 먼저 호출해라! 설정
+    // ==> 이벤트가 발생하면 aop 프락시 객체가 logBefore() 메서드를 먼저 호출함.
+}
---
+++ study/aopex/src/main/java/org/scoula/sample/service/SampleServiceImpl.java
@@ -0,0 +1,11 @@
+package org.scoula.sample.service;
+
+public class SampleServiceImpl implements SampleService {
+
+    @Override
+    public Integer doAdd(String str1, String str2) throws Exception {
+        return Integer.parseInt(str1) + Integer.parseInt(str2);
+        // doAdd("100", "200") --> 100 + 200
+        // doAdd("aaa", "200") --> exception
+    }
+}
